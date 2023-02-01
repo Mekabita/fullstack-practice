1ef88f2bb914d878f5c479b2dcf0f977a3526296
kabitarawal <kabitarawal88@gmail.com>
2023-02-01 22:51:42 +0545

    add axios service to communicate with server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-import axios from "axios"
+import axios from "axios";
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -13,8 +14,8 @@ const App = () => {
   const [checkFilter, setCheckFilter] = useState(false);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    personService.getAll().then((data) => {
+      setPersons(data);
     });
   }, []);
 
@@ -27,9 +28,10 @@ const App = () => {
       const newPerson = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1,
       };
-      setPersons([...persons, newPerson]);
+      personService.create(newPerson).then((data) => {
+        setPersons([...persons, data]);
+      });
     }
   };
 
